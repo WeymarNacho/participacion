9698cf44d1bb6829c1adf85d118476733d8ae364
Weymar Condori Nacho <weymarcondorinacho@gmail.com>
2024-10-24 03:49:59 +0200

    Participacion

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta'  # Necesario para usar sesiones
 
-# Datos simulados de usuarios (esto normalmente estaría en una base de datos)
 users = {
-    "usuario1": "password1",
-    "usuario2": "password2"
+    "Weymar": "123"
 }
 
-# Ruta para la página de inicio de sesión
 @app.route('/', methods=['GET', 'POST'])
-def login():
+def index():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -19,28 +16,28 @@ def login():
         # Validar usuario y contraseña
         if username in users and users[username] == password:
             session['username'] = username  # Guardar usuario en sesión
-            return redirect(url_for('welcome'))  # Redirigir a la página de bienvenida
+            return redirect(url_for('bienvenido'))  # Redirigir a la página de bienvenida
         else:
             flash('Nombre de usuario o contraseña incorrectos.')  # Mostrar error
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
 
-    return render_template('login.html')
+    return render_template('index.html')
 
 # Ruta para la página de bienvenida personalizada
-@app.route('/welcome')
-def welcome():
+@app.route('/bienvenido')
+def bienvenido():
     if 'username' in session:
         username = session['username']  # Obtener el nombre de usuario de la sesión
-        return render_template('welcome.html', username=username)
+        return render_template('bienvenido.html', username=username)
     else:
-        return redirect(url_for('login'))  # Si no está autenticado, redirigir al login
+        return redirect(url_for('index'))  # Si no está autenticado, redirigir al login
 
 # Ruta para cerrar sesión
 @app.route('/logout')
 def logout():
     session.pop('username', None)  # Eliminar usuario de la sesión
     flash('Has cerrado sesión correctamente.')
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
